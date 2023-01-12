8355efe65a333bc1c36a8f0ebd6be1d803980bf8
sergio_serrano <sergioserranorodriguez9@gmail.com>
2023-01-12 17:17:54 +0100

    he hecho la maquetación de login

---
+++ LoginVista.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/LoginVista.css">
+</head>
+<body>
+    <div class="cajaSesión">
+    <form action="">
+        <label for="Usuario">Nombre de usuario</label><br><br>
+        <input type="text" id="usuario" name="usuario" value=""><br><br>
+        <label for="Contraseña">Contraseña</label><br><br>
+        <input type="password" id="contraseña" name="contraseña" value=""><br><br>
+        <input class="boton" type="submit" value="Confirmar">
+    </form> 
+    </div>
+</body>
+</html>
